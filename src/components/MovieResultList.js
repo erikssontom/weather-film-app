9504315927e9f73d8fa8a654b7b9ffa616a24361
Gustav H <hultgrengustav@gmail.com>
2021-05-06 14:01:38 +0200

    Ändrade MovieResultList till en Arrow Function Component

--- src/components/MovieResultList.js
+++ src/components/MovieResultList.js
@@ -1,13 +1,18 @@
-import useFetch from '../utils/useFetch'
-import MovieResult from './MovieResult';
+import useFetch from "../utils/useFetch";
+import MovieResult from "./MovieResult";
 
-export default function MovieResultList({url}) {
-    const { data, isPending, error } = useFetch(url);
-    return (
-        <div>
-            {error && <h1>{error}</h1>}
-            {isPending && <h1>Loading...</h1>}
-           {data && data.Search.map((movie, i) => <MovieResult movie={movie} addToLibEnabled={true} key={i}/>)}
-        </div>
-    )
-}
+const MovieResultList = ({ url }) => {
+  const { data, isPending, error } = useFetch(url);
+  return (
+    <div>
+      {error && <h1>{error}</h1>}
+      {isPending && <h1>Loading...</h1>}
+      {data &&
+        data.Search.map((movie, i) => (
+          <MovieResult movie={movie} addToLibEnabled={true} key={i} />
+        ))}
+    </div>
+  );
+};
+
+export default MovieResultList;
